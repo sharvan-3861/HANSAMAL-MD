a4545c395554d86636f41c47e4ce8e16db4623b7
sharvan 3861 <71188332+sharvan-3861@users.noreply.github.com>
2024-11-23 20:49:50 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -17,9 +17,9 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "IMALKA-HANSAMAL",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "923235489504",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "919494698325",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
